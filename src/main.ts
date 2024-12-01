bb803c5f013cb7e63d0dcab7df633d34b40c1999
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-12-01 13:06:45 +0330

    update => tailwind config & build => font awesome package

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 import { setupCounter } from "./counter.ts"
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="bg-red-500">
-    <h1 class="bg-red-200">Hello</h1>
-    lorem
-  </div>
-`
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = ``
 
 setupCounter(document.querySelector<HTMLButtonElement>("#counter")!)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["index.html", "./src/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "2.65rem",
+          xl: "3.25rem",
+        },
+        screens: {
+          "2xl": "1400px",
+        },
+      },
+    },
   },
   plugins: [],
 }
