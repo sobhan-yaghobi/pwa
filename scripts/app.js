7ea854a7e014665d2f6ee01ea3a39e64a0b66c32
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-12-02 12:05:24 +0330

    remove => the vite config and update the project configs

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("../sw.js")
+      .then((data) => console.log("service worker registered", data))
+      .catch((error) => console.log("service worker is not registered", error))
+  }
+  
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("../sw.ts")
-    .then((data) => console.log("service worker registered", data))
-    .catch((error) => console.log("service worker is not registered", error))
-}
