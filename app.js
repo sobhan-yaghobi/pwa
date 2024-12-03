cd2b639f78fc0431a601f26275828af9ad50dcdd
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-12-03 22:08:47 +0330

    update => write event in app.js file

--- app.js
+++ app.js
@@ -1,6 +1,49 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("sw.js")
-    .then((data) => console.log("service worker registered", data))
-    .catch((error) => console.log("service worker is not registered", error))
+    .then((registration) => {
+      console.log("Service worker registered", registration)
+
+      // 1. Check if Service Worker is in the installing phase
+      if (registration.installing) {
+        console.log("Service Worker installing")
+      }
+
+      // 2. Listen for updates to the Service Worker
+      registration.onupdatefound = () => {
+        const installingWorker = registration.installing
+        if (installingWorker) {
+          installingWorker.onstatechange = () => {
+            switch (installingWorker.state) {
+              case "installed":
+                if (navigator.serviceWorker.controller) {
+                  console.log("New or updated content is available.")
+                } else {
+                  console.log("Content is now available offline!")
+                }
+                break
+              case "activating":
+                console.log("Service Worker activating")
+                break
+              case "activated":
+                console.log("Service Worker activated")
+                break
+              default:
+                break
+            }
+          }
+        }
+      }
+
+      // 3. Check if Service Worker is active
+      if (registration.active) {
+        console.log("Service Worker active")
+      }
+
+      // 4. Check if Service Worker is waiting (new version waiting to activate)
+      if (registration.waiting) {
+        console.log("Service Worker waiting")
+      }
+    })
+    .catch((error) => console.log("Service worker is not registered", error))
 }
--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", (event) => {
-  console.log("service worker has been installed", event)
-})
-
-// activate service worker
-self.addEventListener("activate", (event) =>
-  console.log("service worker has bin activated", event)
-)
